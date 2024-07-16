4c3a4ae027d0c3ad1a3fb4b53936d912f9fc0bad
olepavlsen <olepavlsen@mail.ru>
2024-07-16 10:07:05 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -7,16 +7,12 @@ grades1 = grades[0]
 import statistics
 a1 = statistics.mean(grades1)
 grades2 = grades[1]
-import statistics
 a2 = statistics.mean(grades2)
 grades3 = grades[2]
-import statistics
 a3 = statistics.mean(grades3)
 grades4 = grades[3]
-import statistics
 a4 = statistics.mean(grades4)
 grades5 = grades[4]
-import statistics
 a5 = statistics.mean(grades5)
 average_grades = [a1, a2, a3, a4, a5]
 average_mark = dict(zip(students, average_grades))
